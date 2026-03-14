58a08f4a9f3489d49b855a19a229bcb637cdbf03
milanvadher2003-tech <milanvadher2003@gmail.com>
2026-03-14 16:56:07 +0530

    app.py

--- app.py
+++ app.py
@@ -1,35 +1,103 @@
 import streamlit as st
+import pandas as pd
+import json
 import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 
-st.title("TradingView Chart")
+st.title("Custom TradingView Chart")
 
-html = """
-<div class="tradingview-widget-container">
-  <div id="tradingview_chart"></div>
+uploaded = st.file_uploader("Upload Excel Data", type=["xlsx"])
 
-  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+if uploaded:
 
-  <script type="text/javascript">
-  new TradingView.widget(
-  {
-  "width": "100%",
-  "height": 700,
-  "symbol": "BINANCE:BTCUSDT",
-  "interval": "15",
-  "timezone": "Asia/Kolkata",
-  "theme": "dark",
-  "style": "1",
-  "locale": "en",
-  "toolbar_bg": "#f1f3f6",
-  "enable_publishing": false,
-  "allow_symbol_change": true,
-  "container_id": "tradingview_chart"
-}
-  );
-  </script>
-</div>
-"""
+    df = pd.read_excel(uploaded)
 
-components.html(html, height=720)
+    df["datetime"] = pd.to_datetime(df["datetime"])
+
+    data = []
+
+    for _, r in df.iterrows():
+
+        data.append({
+            "time": int(r["datetime"].timestamp()),
+            "open": float(r["open"]),
+            "high": float(r["high"]),
+            "low": float(r["low"]),
+            "close": float(r["close"]),
+            "volume": float(r["volume"])
+        })
+
+    json_data = json.dumps(data)
+
+    html = f"""
+    <html>
+
+    <head>
+    <script src="static/charting_library/charting_library.js"></script>
+    </head>
+
+    <body>
+
+    <div id="tv_chart_container" style="height:700px"></div>
+
+    <script>
+
+    const data = {json_data};
+
+    const datafeed = {{
+
+        onReady: (cb) => {{
+
+            setTimeout(() => cb({{
+                supported_resolutions: ['1','5','15','60','1D']
+            }}), 0);
+
+        }},
+
+        resolveSymbol: (symbol, cb) => {{
+
+            cb({{
+                name: symbol,
+                type: 'crypto',
+                session: '24x7',
+                timezone: 'Asia/Kolkata',
+                minmov: 1,
+                pricescale: 100,
+                has_intraday: true,
+                supported_resolutions: ['1','5','15','60','1D']
+            }});
+
+        }},
+
+        getBars: (symbolInfo, resolution, from, to, onHistoryCallback) => {{
+
+            const bars = data.filter(b => b.time >= from && b.time <= to);
+
+            onHistoryCallback(bars, {{ noData: false }});
+
+        }}
+
+    }};
+
+    new TradingView.widget({{
+
+        container_id: "tv_chart_container",
+        autosize: true,
+        symbol: "CUSTOM",
+        interval: "15",
+        datafeed: datafeed,
+        library_path: "static/charting_library/",
+        locale: "en",
+        theme: "dark"
+
+    }});
+
+    </script>
+
+    </body>
+
+    </html>
+    """
+
+    components.html(html, height=720)
