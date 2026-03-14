2f7bad9974768571697cf818a3772713535dac3b
milanvadher2003-tech <milanvadher2003@gmail.com>
2026-03-14 17:08:49 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from streamlit_lightweight_charts import renderLightweightCharts
 
 st.set_page_config(layout="wide")
 
-st.title("TradingView Style Candlestick")
+st.title("TradingView Candlestick")
 
 file = st.file_uploader("Upload Excel", type=["xlsx"])
 
@@ -12,44 +12,35 @@ if file:
 
     df = pd.read_excel(file)
 
+    # datetime convert
     df["datetime"] = pd.to_datetime(df["datetime"])
 
+    # sort
     df = df.sort_values("datetime")
 
-    # UNIX timestamp (seconds)
-    df["time"] = df["datetime"].astype("int64") // 10**9
+    # convert to seconds timestamp
+    df["time"] = df["datetime"].apply(lambda x: int(x.timestamp()))
 
     data = []
 
-    for _, r in df.iterrows():
+    for i in range(len(df)):
 
         data.append({
-            "time": int(r["time"]),
-            "open": float(r["open"]),
-            "high": float(r["high"]),
-            "low": float(r["low"]),
-            "close": float(r["close"])
+            "time": df.iloc[i]["time"],
+            "open": float(df.iloc[i]["open"]),
+            "high": float(df.iloc[i]["high"]),
+            "low": float(df.iloc[i]["low"]),
+            "close": float(df.iloc[i]["close"])
         })
 
     chart = [{
         "chart": {
-            "height": 700,
-            "layout": {
-                "background": {"color": "#000000"},
-                "textColor": "#DDD"
-            }
+            "height": 700
         },
         "series": [
             {
                 "type": "Candlestick",
-                "data": data,
-                "options": {
-                    "upColor": "#26a69a",
-                    "downColor": "#ef5350",
-                    "borderVisible": False,
-                    "wickUpColor": "#26a69a",
-                    "wickDownColor": "#ef5350"
-                }
+                "data": data
             }
         ]
     }]
