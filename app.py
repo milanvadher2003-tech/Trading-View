cc1ecd09298200176861c8a41d67a4d971bd5dee
milanvadher2003-tech <milanvadher2003@gmail.com>
2026-03-14 17:21:49 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from streamlit_lightweight_charts import renderLightweightCharts
 
 st.set_page_config(layout="wide")
 
-st.title("TradingView Style Candlestick")
+st.title("TradingView Candlestick")
 
 file = st.file_uploader("Upload Excel", type=["xlsx"])
 
@@ -12,44 +12,38 @@ if file:
 
     df = pd.read_excel(file)
 
-    # clean columns
     df.columns = df.columns.str.strip()
 
-    # datetime convert
     df["datetime"] = pd.to_datetime(df["datetime"])
 
-    # sort
     df = df.sort_values("datetime")
 
-    # convert to ISO format
-    df["time"] = df["datetime"].dt.strftime("%Y-%m-%dT%H:%M:%S")
+    df["time"] = df["datetime"].dt.strftime("%Y-%m-%d %H:%M:%S")
 
     data = []
 
     for _, r in df.iterrows():
 
         data.append({
-            "time": str(r["time"]),
+            "time": r["time"],
             "open": float(r["open"]),
             "high": float(r["high"]),
             "low": float(r["low"]),
             "close": float(r["close"])
         })
 
-    chart = [{
-        "chart": {
-            "height": 700,
-            "layout": {
-                "background": {"color": "#000000"},
-                "textColor": "#DDD"
-            }
-        },
-        "series": [
-            {
-                "type": "Candlestick",
-                "data": data
-            }
-        ]
-    }]
+    chart = [
+        {
+            "chart": {
+                "height": 700
+            },
+            "series": [
+                {
+                    "type": "candlestick",
+                    "data": data
+                }
+            ]
+        }
+    ]
 
     renderLightweightCharts(chart)
